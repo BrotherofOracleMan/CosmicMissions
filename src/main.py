feed78987c94f4e80acd362ff97769976505cb5e
BrotherofOracleMan <jsiow002.dev@gmail.com>
2026-07-20 15:49:58 -0700

    removed unnecessart imports from main.py

--- src/main.py
+++ src/main.py
@@ -1,10 +1,5 @@
-from fastapi import Depends, FastAPI, HTTPException
-from sqlalchemy.exc import IntegrityError
-from sqlalchemy.orm import Session
+from fastapi import  FastAPI
 
-from database import get_db
-from models import CosmicMission
-from schemas import CosmicMissionBase, CosmicMissionCreate, CosmicMissionPut, CosmicMissionUpdate
 from routers import router as cosmic_missions_router
 
 app=FastAPI()
